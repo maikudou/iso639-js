27e6d2695cd0c38638cbf8b9be6c385ecd61d6e1
Mikhail Labanov <maikudou@gmail.com>
2015-09-29 19:19:28 +0300

    Added native languages names

---
+++ tools/createAlpha3toNativeNameMapping.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+
+var allCodes = fs.readFileSync('reference/iso639-3.json', 'utf8');
+var allCodeParsed = JSON.parse(allCodes);
+
+var allNativeNameCodes = fs.readFileSync('reference/iso639-3_native-name_partial.json', 'utf8');
+var allNativeNameCodesParsed = JSON.parse(allNativeNameCodes);
+
+var mapping = {};
+
+for(languageCode in allCodeParsed){
+    if(allNativeNameCodesParsed[languageCode]){
+        mapping[languageCode] = {};
+        mapping[languageCode].nativeName = allNativeNameCodesParsed[languageCode].nativeName;
+        mapping[languageCode].englishName = allNativeNameCodesParsed[languageCode].englishName.split(',')[0];
+    }
+}
+
+fs.writeFileSync('alpha3toNativeNameMapping.json', JSON.stringify(mapping, null, '    '), 'utf8');
